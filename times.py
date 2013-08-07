ea0d973cde695e81e11a79fd27fe6fd3459ae5f2
Eagelt Jhang <shuyue@KimtekiMacBook-Air.local>
2013-08-07 14:18:44 +0800

    New

---
+++ times.py
@@ -0,0 +1,7 @@
+print("Mnemonics for multiplication timetables")
+print()
+for i in range(1, 10):
+        print(i,"times x")
+        for j in range(i,10):
+                print(i,"*",j,"=",i*j)
+        print()
